4f5c7a3c65f09d1c8c3ecd9bfde343619eb30610
Mubeen16 <medifize16@gmail.com>
2025-12-15 23:35:33 +0000

    Implement exit builtin

--- app/main.py
+++ app/main.py
@@ -3,10 +3,19 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
-        sys.stdout.flush()
+        sys.stdout.flush() # for exiting the buffer
 
-        command = input()
-        print(f"{command}: command not found")
+        line = sys.stdin.readline() #input from user saved in line
+        if not line:
+            continue
+
+        command = line.strip()
+        if command == "exit":
+            break # terminate the shell
+
+        if command: # else print this below
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
+
