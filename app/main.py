b93cc09be6cf9e8d6602132f7a704e2a49febc2a
Mubeen16 <medifize16@gmail.com>
2025-12-16 19:19:55 +0000

    Extend type to search PATH

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 BUILTINS = {"exit", "echo", "type"}
 
@@ -28,11 +29,23 @@ def main():
             if not args:
                 continue
             target = args[0]
+
+            # 1.builtin check
             if target in BUILTINS:
                 print(f"{target} is a shell builtin")
+                continue
+
+            # 2. PATH search
+            for directory in os.environ.get("PATH", "").split(os.pathsep):
+                full_path = os.path.join(directory, target)
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    print(f"{target} is {full_path}")
+                    break
+                    
             else:
                 print(f"{target}: not found")
             continue
+
 
         print(f"{cmd}: command not found")
 
